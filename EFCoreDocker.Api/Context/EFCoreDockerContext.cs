5e70539a141d577d0d15f6a767b20cd6c62e6755
Bufaonanet <douglas.bufaonanet@outlook.com>
2021-09-23 10:03:47 -0300

    first commit

---
+++ EFCoreDocker.Api/Context/EFCoreDockerContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreDocker.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDocker.Api.Context
+{
+    public class EFCoreDockerContext : DbContext
+    {
+        public EFCoreDockerContext(DbContextOptions<EFCoreDockerContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
